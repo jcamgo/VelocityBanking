0248f356359d12500a69de804f156c0dfc7d10d6
Jonny Camargo <jonnycamargo96@gmail.com>
2018-08-19 17:14:38 -0600

    Model Files

---
+++ Expenses.swift
@@ -0,0 +1,9 @@
+//
+//  Expenses.swift
+//  VelocityBanking
+//
+//  Created by jonathan orellana on 8/19/18.
+//  Copyright © 2018 jonathan orellana. All rights reserved.
+//
+
+import Foundation
---
+++ Income.swift
@@ -0,0 +1,9 @@
+//
+//  Income.swift
+//  VelocityBanking
+//
+//  Created by jonathan orellana on 8/19/18.
+//  Copyright © 2018 jonathan orellana. All rights reserved.
+//
+
+import Foundation
